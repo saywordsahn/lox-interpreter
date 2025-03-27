5ceee05c5ac2b3ff4d09a600754f847a99c87733
saywordsahn <saywordsahn@gmail.com>
2025-03-27 15:39:20 +0900

    Added other single tokens

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import enum
 
 tokens = {
     '(': 'LEFT_PAREN',
-    ')': 'RIGHT_PAREN'
+    ')': 'RIGHT_PAREN',
+    '{': 'LEFT_BRACE',
+    '}': 'RIGHT_BRACE',
+    '+': 'PLUS',
+    '-': 'MINUS',
+    '*': 'STAR',
+    '.': 'DOT',
+    ',': 'COMMA'
 }
 
 with open("program.lox", "r") as file:
     content = file.read()
+    print(content)
 
 start = 0
 end = len(content)
